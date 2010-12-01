ac80199d5c802cbd81eeaa2baded32cf7c0da858
Allan CORNET <allan.cornet@scilab.org>
2010-12-01 10:41:23 +0100

    add file

---
+++ sci_gateway/cpp/sci_db.cxx
@@ -0,0 +1,32 @@
+/* ==================================================================== */
+/* Igor GRICHYN */
+/* Scidb toolbox */
+/* This file is released into the public domain */
+/* ==================================================================== */
+
+/* ==================================================================== */
+#include "sci_db.h"
+/* ==================================================================== */
+
+QString sDefaultConnection = "";
+
+QMap< QString,QList<QString> > mslsProviderConnectionOptions = QMap< QString, QList<QString> >();
+
+QList<QString> lsCommonConnectionParameters = QList<QString>()
+		<<"host"
+		<<"port"
+		<<"database"
+		<<"user"
+		<<"provider"
+		<<"password";
+	
+QList<QString> lsProviders = QList<QString>()
+		<<"QPSQL"
+		<<"QSQLITE"
+		<<"QMYSQL"
+		<<"QOCI"
+		<<"QIBASE"
+		<<"QDB2"
+		<<"QODBC"
+		<<"QSQLITE2"
+		<<"QTDS";
